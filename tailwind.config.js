55bd0bbac98a759db63451ff1007390179bedb05
Karl Horky <karl.horky@gmail.com>
2023-08-06 17:08:05 +0200

    Fix type-checking

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
@@ -29,3 +29,5 @@ module.exports = {
   },
   plugins: [],
 };
+
+module.exports = config;
