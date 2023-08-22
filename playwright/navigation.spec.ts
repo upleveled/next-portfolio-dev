da3e704f4267f6aead4142e5cb4052c94b5647f9
Lukas Prochazka <prochazka.lu@gmail.com>
2023-08-22 21:21:44 +0200

    Update navigation test

--- playwright/navigation.spec.ts
+++ playwright/navigation.spec.ts
@@ -1,8 +1,17 @@
 import { expect, test } from '@playwright/test';
 
-test('Check content', async ({ page }) => {
+test('Navigate and check content', async ({ page }) => {
   await page.goto('/');
   await expect(page.getByTestId('intro')).toBeVisible();
   await expect(page.getByTestId('projects')).toBeVisible();
   await expect(page.getByTestId('skills')).toBeVisible();
+
+  await page.getByRole('link', { name: 'About' }).click();
+  await page.waitForURL('/#about');
+
+  await page.getByRole('link', { name: 'Projects' }).click();
+  await page.waitForURL('/#projects');
+
+  await page.getByRole('link', { name: 'Contact' }).click();
+  await page.waitForURL('/#contact');
 });
