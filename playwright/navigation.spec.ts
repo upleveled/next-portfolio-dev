8dd2cdfaa9c0abe60012626132c968532ab39d76
Lukas Prochazka <prochazka.lu@gmail.com>
2023-08-22 15:39:02 +0200

    Update test

--- playwright/navigation.spec.ts
+++ playwright/navigation.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
-test('Can find the different components in the app', async ({ page }) => {
+test('Check content', async ({ page }) => {
   await page.goto('/');
   await expect(page.getByTestId('intro')).toBeVisible();
   await expect(page.getByTestId('projects')).toBeVisible();
