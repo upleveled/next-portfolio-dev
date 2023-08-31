73981b4b2a64b0a0dd84356ec38d6f995982e6d7
Lukas Prochazka <prochazka.lu@gmail.com>
2023-08-31 10:45:39 +0200

    Add navigation assertion

--- playwright/navigation.spec.ts
+++ playwright/navigation.spec.ts
@@ -5,13 +5,17 @@ test('Navigate and check content', async ({ page }) => {
   await expect(page.getByTestId('intro')).toBeVisible();
   await expect(page.getByTestId('projects')).toBeVisible();
   await expect(page.getByTestId('skills')).toBeVisible();
+  await expect(page.getByTestId('contact')).toBeVisible();
 
   await page.getByRole('link', { name: 'About' }).click();
   await page.waitForURL('/#about');
+  await expect(page.getByTestId('skills')).toBeInViewport();
 
   await page.getByRole('link', { name: 'Projects' }).click();
   await page.waitForURL('/#projects');
+  await expect(page.getByTestId('projects')).toBeInViewport();
 
   await page.getByRole('link', { name: 'Contact' }).click();
   await page.waitForURL('/#contact');
+  await expect(page.getByTestId('contact')).toBeInViewport();
 });
