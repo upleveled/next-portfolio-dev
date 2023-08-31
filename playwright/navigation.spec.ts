2be8c0582bd0fdb2b2680c70d059966c1a792d02
Lukas Prochazka <prochazka.lu@gmail.com>
2023-08-31 16:24:09 +0200

    Change skills to about for data-test-id

--- playwright/navigation.spec.ts
+++ playwright/navigation.spec.ts
@@ -4,12 +4,12 @@ test('Navigate and check content', async ({ page }) => {
   await page.goto('/');
   await expect(page.getByTestId('intro')).toBeVisible();
   await expect(page.getByTestId('projects')).toBeVisible();
-  await expect(page.getByTestId('skills')).toBeVisible();
+  await expect(page.getByTestId('about')).toBeVisible();
   await expect(page.getByTestId('contact')).toBeVisible();
 
   await page.getByRole('link', { name: 'About' }).click();
   await page.waitForURL('/#about');
-  await expect(page.getByTestId('skills')).toBeInViewport();
+  await expect(page.getByTestId('about')).toBeInViewport();
 
   await page.getByRole('link', { name: 'Projects' }).click();
   await page.waitForURL('/#projects');
