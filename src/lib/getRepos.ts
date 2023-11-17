bc779ba4b5fa0b94912bcc9ebff35a3c741a11ff
Karl Horky <karl.horky@gmail.com>
2023-11-17 11:08:04 +0100

    Fix config and indentation

--- src/lib/getRepos.ts
+++ src/lib/getRepos.ts
@@ -2,10 +2,10 @@ import 'server-only';
 import { RepositoryEdge } from '../generated/graphql';
 
 const getRepos = async (): Promise<RepositoryEdge[]> => {
-	const res = await fetch('https://api.github.com/graphql', {
-		method: 'POST',
-		body: JSON.stringify({
-			query: `
+  const res = await fetch('https://api.github.com/graphql', {
+    method: 'POST',
+    body: JSON.stringify({
+      query: `
 				query viewer {
 					viewer {
 						repositories(first: 8, orderBy: {field: STARGAZERS, direction: DESC}) {
@@ -31,20 +31,20 @@ const getRepos = async (): Promise<RepositoryEdge[]> => {
 					}
 				}
 			`,
-		}),
-		headers: {
-			Authorization: `bearer ${process.env.GITHUB_TOKEN}`,
-		},
-		next: { revalidate: 10 },
-	});
+    }),
+    headers: {
+      Authorization: `bearer ${process.env.GITHUB_TOKEN}`,
+    },
+    next: { revalidate: 10 },
+  });
 
-	if (!res.ok) {
-		throw new Error('Failed to fetch data');
-	}
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
 
-	const data = await res.json();
+  const data = await res.json();
 
-	return data.data.viewer.repositories.edges;
+  return data.data.viewer.repositories.edges;
 };
 
 export default getRepos;
