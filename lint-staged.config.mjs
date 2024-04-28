c0527f7b6b2be44129688b88f79063dd19cdd588
Lukas Prochazka <prochazka.lu@gmail.com>
2024-04-28 17:07:12 +0200

    Fix lint-staged.config.mjs to include JSDoc for buildEslintCommand function

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,5 +1,9 @@
 const path = require('node:path');
 
+/**
+ * @param {string[]} filenames
+ */
+
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
