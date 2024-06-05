af865707fd5d4286d624392c9eb1c554a968191e
Lukas Prochazka <prochazka.lu@gmail.com>
2024-06-05 12:03:21 +0200

    Update github-schema-loader to use ES modules

--- src/generated/github-schema-loader.js
+++ src/generated/github-schema-loader.js
@@ -1,6 +1,5 @@
+import '@graphql-codegen/typescript';
+import '@graphql-codegen/typescript-operations';
 import { schema } from '@octokit/graphql-schema';
-
-import('@graphql-codegen/typescript');
-import('@graphql-codegen/typescript-operations');
 
 export default schema.json;
