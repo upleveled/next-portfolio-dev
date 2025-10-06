8b009589b75feac5b7a59cacb14e0902e7769963
Karl Horky <karl.horky@gmail.com>
2025-10-06 18:42:53 +0200

    Switch to soon-allowed ref pattern for react-hooks/set-state-in-effect
    - https://github.com/facebook/react/issues/34743#issuecomment-3369674484

--- src/components/ui/theme/Header/NavbarLinks/index.tsx
+++ src/components/ui/theme/Header/NavbarLinks/index.tsx
@@ -3,7 +3,7 @@
 import clsx from 'clsx';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import ToggleTheme from '../ToggleTheme';
 
 type NavbarLinksProps = {
@@ -11,11 +11,13 @@ type NavbarLinksProps = {
 };
 
 const NavbarLinks = ({ desktop }: NavbarLinksProps) => {
+  const ref = useRef<HTMLDivElement>(null);
   const [hasMounted, setHasMounted] = useState(false);
   const pathname = usePathname();
 
   useEffect(() => {
-    setHasMounted(true);
+    // eslint-disable-next-line react-hooks/set-state-in-effect -- TODO: Re-enable when eslint-config-upleveled releases fixed version https://github.com/upleveled/eslint-config-upleveled/pull/554
+    setHasMounted(!!ref.current);
   }, []);
 
   return (
@@ -24,6 +26,7 @@ const NavbarLinks = ({ desktop }: NavbarLinksProps) => {
         'items-center hidden lg:flex': desktop,
         'p-12 flex flex-col': !desktop,
       })}
+      ref={ref}
     >
       <Link
         href={`${pathname}#about`}
